f959d5b5f1efbb149718d2d2da3e0dfe78fe02c7
Ashik-Rahman100 <ashik5515@gmail.com>
2023-05-31 18:46:13 +0600

    eslint, prettier, husky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import express, { Application, Request } from 'express'
+import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 
 // cors setup
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Working succesfully')
 })
 
